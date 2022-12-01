e9a0d696e28ff521e02b1b3964ed7b97250cc98b
rsat100 <rsatbudiman@gmail.com>
2022-12-01 18:14:23 +0700

    01/12/2022

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store, { persistedStore } from "/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistedStore} loading={null}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
