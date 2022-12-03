5372f4831a7341dd2f7690694fbb03370c446786
rofa <zainrofa29@gmail.com>
2022-12-04 01:20:45 +0700

    add component card ticket

---
+++ Components/CardTicket.js
@@ -0,0 +1,318 @@
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/CardTicket.module.css";
+import cinema from "../assets/sponsor-1.png";
+
+function CardTicket() {
+  return (
+    <>
+      <div className={styles["card-ticket"]}>
+        <div className={styles["top-container"]}>
+          <div className={styles["image-container"]}>
+            <Image src={cinema} alt="logo" layout="fill" objectFit="contain" />
+          </div>
+          <div className={styles["details"]}>
+            <h1>ebv.id</h1>
+            <p>Downcare street No. 21, East Purwokerto</p>
+          </div>
+        </div>
+        <div className={styles["schedule"]}>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+        </div>
+        <div className={styles["price-container"]}>
+          <div className={styles["title"]}>
+            <p>Price</p>
+          </div>
+          <div className={styles["price"]}>
+            <p>Rp. 25.000/seat</p>
+          </div>
+        </div>
+        <div className={styles["button-container"]}>
+          <div className={styles["book-btn"]}>
+            <button>Book now</button>
+          </div>
+          <div className={styles["add-btn"]}>
+            <button>Add to cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["card-ticket"]}>
+        <div className={styles["top-container"]}>
+          <div className={styles["image-container"]}>
+            <Image src={cinema} alt="logo" layout="fill" objectFit="contain" />
+          </div>
+          <div className={styles["details"]}>
+            <h1>ebv.id</h1>
+            <p>Downcare street No. 21, East Purwokerto</p>
+          </div>
+        </div>
+        <div className={styles["schedule"]}>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+        </div>
+        <div className={styles["price-container"]}>
+          <div className={styles["title"]}>
+            <p>Price</p>
+          </div>
+          <div className={styles["price"]}>
+            <p>Rp. 25.000/seat</p>
+          </div>
+        </div>
+        <div className={styles["button-container"]}>
+          <div className={styles["book-btn"]}>
+            <button>Book now</button>
+          </div>
+          <div className={styles["add-btn"]}>
+            <button>Add to cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["card-ticket"]}>
+        <div className={styles["top-container"]}>
+          <div className={styles["image-container"]}>
+            <Image src={cinema} alt="logo" layout="fill" objectFit="contain" />
+          </div>
+          <div className={styles["details"]}>
+            <h1>ebv.id</h1>
+            <p>Downcare street No. 21, East Purwokerto</p>
+          </div>
+        </div>
+        <div className={styles["schedule"]}>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+        </div>
+        <div className={styles["price-container"]}>
+          <div className={styles["title"]}>
+            <p>Price</p>
+          </div>
+          <div className={styles["price"]}>
+            <p>Rp. 25.000/seat</p>
+          </div>
+        </div>
+        <div className={styles["button-container"]}>
+          <div className={styles["book-btn"]}>
+            <button>Book now</button>
+          </div>
+          <div className={styles["add-btn"]}>
+            <button>Add to cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["card-ticket"]}>
+        <div className={styles["top-container"]}>
+          <div className={styles["image-container"]}>
+            <Image src={cinema} alt="logo" layout="fill" objectFit="contain" />
+          </div>
+          <div className={styles["details"]}>
+            <h1>ebv.id</h1>
+            <p>Downcare street No. 21, East Purwokerto</p>
+          </div>
+        </div>
+        <div className={styles["schedule"]}>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+        </div>
+        <div className={styles["price-container"]}>
+          <div className={styles["title"]}>
+            <p>Price</p>
+          </div>
+          <div className={styles["price"]}>
+            <p>Rp. 25.000/seat</p>
+          </div>
+        </div>
+        <div className={styles["button-container"]}>
+          <div className={styles["book-btn"]}>
+            <button>Book now</button>
+          </div>
+          <div className={styles["add-btn"]}>
+            <button>Add to cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["card-ticket"]}>
+        <div className={styles["top-container"]}>
+          <div className={styles["image-container"]}>
+            <Image src={cinema} alt="logo" layout="fill" objectFit="contain" />
+          </div>
+          <div className={styles["details"]}>
+            <h1>ebv.id</h1>
+            <p>Downcare street No. 21, East Purwokerto</p>
+          </div>
+        </div>
+        <div className={styles["schedule"]}>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+        </div>
+        <div className={styles["price-container"]}>
+          <div className={styles["title"]}>
+            <p>Price</p>
+          </div>
+          <div className={styles["price"]}>
+            <p>Rp. 25.000/seat</p>
+          </div>
+        </div>
+        <div className={styles["button-container"]}>
+          <div className={styles["book-btn"]}>
+            <button>Book now</button>
+          </div>
+          <div className={styles["add-btn"]}>
+            <button>Add to cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["card-ticket"]}>
+        <div className={styles["top-container"]}>
+          <div className={styles["image-container"]}>
+            <Image src={cinema} alt="logo" layout="fill" objectFit="contain" />
+          </div>
+          <div className={styles["details"]}>
+            <h1>ebv.id</h1>
+            <p>Downcare street No. 21, East Purwokerto</p>
+          </div>
+        </div>
+        <div className={styles["schedule"]}>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+          <div className={styles["time"]}>
+            <p>08:30am</p>
+          </div>
+        </div>
+        <div className={styles["price-container"]}>
+          <div className={styles["title"]}>
+            <p>Price</p>
+          </div>
+          <div className={styles["price"]}>
+            <p>Rp. 25.000/seat</p>
+          </div>
+        </div>
+        <div className={styles["button-container"]}>
+          <div className={styles["book-btn"]}>
+            <button>Book now</button>
+          </div>
+          <div className={styles["add-btn"]}>
+            <button>Add to cart</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CardTicket;
