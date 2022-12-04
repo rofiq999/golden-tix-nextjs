a6e956a7db872787e60bd158b3cb7d8381fd15aa
rofa <zainrofa29@gmail.com>
2022-12-04 20:33:19 +0700

    imple showung and upcoming

--- Components/CardMovieShow.js
+++ Components/CardMovieShow.js
@@ -3,13 +3,14 @@ import Image from "next/image";
 import styles from "../styles/CardMovieShow.module.css";
 import sample from "../assets/poster1.png";
 
-function CardMovieShow() {
+function CardMovieShow({ name, categories, image }) {
+  const link = process.env.NEXT_PUBLIC_CLOUDINARY_LINK;
   return (
     <>
       <div className={styles["card"]}>
         <div className={styles["image-container"]}>
           <Image
-            src={sample}
+            src={`${link}/${image}`}
             alt="poster"
             layout="fill"
             objectFit="contain"
@@ -17,110 +18,10 @@ function CardMovieShow() {
         </div>
         <div className={styles["card-movie"]}>
           <div className={styles["title"]}>
-            <p>Manusya Bala-Bala Balik Rumah</p>
+            <p>{name}</p>
           </div>
           <div className={styles["categories"]}>
-            <p>Acion, Adventure, Sci-FI</p>
-          </div>
-          <div className={styles["detail-btn"]}>
-            <button>Details</button>
-          </div>
-          <div className={styles["book-btn"]}>
-            <button>Book Now</button>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles["card"]}>
-        <div className={styles["image-container"]}>
-          <Image
-            src={sample}
-            alt="poster"
-            layout="fill"
-            objectFit="contain"
-          ></Image>
-        </div>
-        <div className={styles["card-movie"]}>
-          <div className={styles["title"]}>
-            <p>Manusya Bala-Bala Balik Rumah</p>
-          </div>
-          <div className={styles["categories"]}>
-            <p>Acion, Adventure, Sci-FI</p>
-          </div>
-          <div className={styles["detail-btn"]}>
-            <button>Details</button>
-          </div>
-          <div className={styles["book-btn"]}>
-            <button>Book Now</button>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles["card"]}>
-        <div className={styles["image-container"]}>
-          <Image
-            src={sample}
-            alt="poster"
-            layout="fill"
-            objectFit="contain"
-          ></Image>
-        </div>
-        <div className={styles["card-movie"]}>
-          <div className={styles["title"]}>
-            <p>Manusya Bala-Bala Balik Rumah</p>
-          </div>
-          <div className={styles["categories"]}>
-            <p>Acion, Adventure, Sci-FI</p>
-          </div>
-          <div className={styles["detail-btn"]}>
-            <button>Details</button>
-          </div>
-          <div className={styles["book-btn"]}>
-            <button>Book Now</button>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles["card"]}>
-        <div className={styles["image-container"]}>
-          <Image
-            src={sample}
-            alt="poster"
-            layout="fill"
-            objectFit="contain"
-          ></Image>
-        </div>
-        <div className={styles["card-movie"]}>
-          <div className={styles["title"]}>
-            <p>Manusya Bala-Bala Balik Rumah</p>
-          </div>
-          <div className={styles["categories"]}>
-            <p>Acion, Adventure, Sci-FI</p>
-          </div>
-          <div className={styles["detail-btn"]}>
-            <button>Details</button>
-          </div>
-          <div className={styles["book-btn"]}>
-            <button>Book Now</button>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles["card"]}>
-        <div className={styles["image-container"]}>
-          <Image
-            src={sample}
-            alt="poster"
-            layout="fill"
-            objectFit="contain"
-          ></Image>
-        </div>
-        <div className={styles["card-movie"]}>
-          <div className={styles["title"]}>
-            <p>Manusya Bala-Bala Balik Rumah</p>
-          </div>
-          <div className={styles["categories"]}>
-            <p>Acion, Adventure, Sci-FI</p>
+            <p>{categories}</p>
           </div>
           <div className={styles["detail-btn"]}>
             <button>Details</button>
--- Components/CardUpcoming.js
+++ Components/CardUpcoming.js
@@ -3,123 +3,25 @@ import Image from "next/image";
 import styles from "../styles/CardUpcoming.module.css";
 import sample from "../assets/poster1.png";
 
-function CardUpcoming() {
+function CardUpcoming({ name, category, image }) {
+  const link = process.env.NEXT_PUBLIC_CLOUDINARY_LINK;
+
   return (
     <>
       <div className={styles["card-movie"]}>
         <div className={styles["image-container"]}>
           <Image
-            src={sample}
+            src={`${link}/${image}`}
             alt="poster"
             layout="fill"
             objectFit="contain"
           ></Image>
         </div>
         <div className={styles["title"]}>
-          <p>Manusya Bala-Bala Balik Rumah</p>
+          <p>{name}</p>
         </div>
         <div className={styles["categories"]}>
-          <p>Acion, Adventure, Sci-FI</p>
-        </div>
-        <div className={styles["detail-btn"]}>
-          <button>Details</button>
-        </div>
-      </div>
-
-      <div className={styles["card-movie"]}>
-        <div className={styles["image-container"]}>
-          <Image
-            src={sample}
-            alt="poster"
-            layout="fill"
-            objectFit="contain"
-          ></Image>
-        </div>
-        <div className={styles["title"]}>
-          <p>Manusya Bala-Bala Balik Rumah</p>
-        </div>
-        <div className={styles["categories"]}>
-          <p>Acion, Adventure, Sci-FI</p>
-        </div>
-        <div className={styles["detail-btn"]}>
-          <button>Details</button>
-        </div>
-      </div>
-
-      <div className={styles["card-movie"]}>
-        <div className={styles["image-container"]}>
-          <Image
-            src={sample}
-            alt="poster"
-            layout="fill"
-            objectFit="contain"
-          ></Image>
-        </div>
-        <div className={styles["title"]}>
-          <p>Manusya Bala-Bala Balik Rumah</p>
-        </div>
-        <div className={styles["categories"]}>
-          <p>Acion, Adventure, Sci-FI</p>
-        </div>
-        <div className={styles["detail-btn"]}>
-          <button>Details</button>
-        </div>
-      </div>
-
-      <div className={styles["card-movie"]}>
-        <div className={styles["image-container"]}>
-          <Image
-            src={sample}
-            alt="poster"
-            layout="fill"
-            objectFit="contain"
-          ></Image>
-        </div>
-        <div className={styles["title"]}>
-          <p>Manusya Bala-Bala Balik Rumah</p>
-        </div>
-        <div className={styles["categories"]}>
-          <p>Acion, Adventure, Sci-FI</p>
-        </div>
-        <div className={styles["detail-btn"]}>
-          <button>Details</button>
-        </div>
-      </div>
-
-      <div className={styles["card-movie"]}>
-        <div className={styles["image-container"]}>
-          <Image
-            src={sample}
-            alt="poster"
-            layout="fill"
-            objectFit="contain"
-          ></Image>
-        </div>
-        <div className={styles["title"]}>
-          <p>Manusya Bala-Bala Balik Rumah</p>
-        </div>
-        <div className={styles["categories"]}>
-          <p>Acion, Adventure, Sci-FI</p>
-        </div>
-        <div className={styles["detail-btn"]}>
-          <button>Details</button>
-        </div>
-      </div>
-
-      <div className={styles["card-movie"]}>
-        <div className={styles["image-container"]}>
-          <Image
-            src={sample}
-            alt="poster"
-            layout="fill"
-            objectFit="contain"
-          ></Image>
-        </div>
-        <div className={styles["title"]}>
-          <p>Manusya Bala-Bala Balik Rumah</p>
-        </div>
-        <div className={styles["categories"]}>
-          <p>Acion, Adventure, Sci-FI</p>
+          <p>{category}</p>
         </div>
         <div className={styles["detail-btn"]}>
           <button>Details</button>
