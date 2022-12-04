343dd049751be4b7444f890e5817ad1d524c06cf
rofiq999 <rofiqhidayat495@gmail.com>
2022-12-05 00:50:17 +0700

    update

--- pages/history/index.js
+++ pages/history/index.js
@@ -1,5 +1,3 @@
-// import React from 'react'
-// import React, { useEffect, useState } from "react";
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from "next/router";
