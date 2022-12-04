ea59f6eb6d26f2f9c62fad2d187cfd229f74ff9a
rofa <zainrofa29@gmail.com>
2022-12-04 23:40:12 +0700

    add config axios

--- modules/api/movies.js
+++ modules/api/movies.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-const axiosRequest = (method, url, data, params) => {
+const axiosRequest = (method, url, data, params, id) => {
   return axios({
     method,
     url: `${baseUrl}${url}`,
@@ -11,10 +11,22 @@ const axiosRequest = (method, url, data, params) => {
   });
 };
 
+const config = (token) => {
+  return {
+    headers: {
+      "x-access-token": token,
+    },
+  };
+};
+
 export const getMovieShowing = (params) => {
   return axiosRequest("GET", "/api/movie/showing", params);
 };
 
 export const getMovieUpcoming = (params) => {
   return axiosRequest("GET", "/api/movie/upcoming", params);
+};
+
+export const getMovieDetails = (token, params) => {
+  return axios.get(`${baseUrl}${params}`, config(token));
 };
