dc6e85e1c2eb53de94b8850718082a45bcba1f77
rsat100 <rsatbudiman@gmail.com>
2022-12-06 09:42:18 +0700

    06/12/2022

--- pages/order/index.js
+++ pages/order/index.js
@@ -643,13 +643,13 @@ function Order() {
                       <p>7</p>
                     </div>
                     <div className={styles["seat-div-10"]}>
-                      <p>1</p>
-                      <p>2</p>
-                      <p>3</p>
-                      <p>4</p>
-                      <p>5</p>
-                      <p>6</p>
-                      <p>7</p>
+                      <p>8</p>
+                      <p>9</p>
+                      <p>10</p>
+                      <p>11</p>
+                      <p>12</p>
+                      <p>13</p>
+                      <p>14</p>
                     </div>
                   </div>
                   <div className={styles["seat-div-19"]}>
